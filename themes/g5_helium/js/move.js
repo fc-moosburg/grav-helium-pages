21e5f71471bbfb14e9e59930da3271a5614d5bd7
Janis Reinelt <dev@reinelt.online>
2021-11-28 15:58:17 +0000

    Fix missing ID (widget)

--- themes/g5_helium/js/move.js
+++ themes/g5_helium/js/move.js
@@ -1,20 +1,24 @@
 console.log("Moving things...");
+function fcmMove() {
 
-selector = "fcm-move-to-";
+    selector = "fcm-move-to-";
 
-items_to_move = jQuery(`[class*='${selector}']`);
-// console.log(items_to_move);
+    items_to_move = jQuery(`[class*='${selector}']`);
+    // console.log(items_to_move);
 
-items_to_move.each((i, widget) => {
-    let classes = jQuery(widget).attr("class").split(/\s+/);
-    let id_to_move_to = classes.filter(c => c.startsWith(selector))[0].replace(selector, "");
-    let ids = jQuery(`#${id_to_move_to}`);
-    if (ids && ids.length > 0) {
-        jQuery(widget).detach();
-        ids[0].appendChild(widget);
-    } else {
-        console.warn("Unable to find", id_to_move_to);
-    }
-    console.log(i, widget, jQuery(widget), classes, id_to_move_to);
-});
+    items_to_move.each((i, widget) => {
+        let classes = jQuery(widget).attr("class").split(/\s+/);
+        let id_to_move_to = classes.filter(c => c.startsWith(selector))[0].replace(selector, "");
+        let ids = jQuery(`#${id_to_move_to}`);
+        if (ids && ids.length > 0) {
+            jQuery(widget).detach();
+            ids[0].appendChild(widget);
+        } else {
+            console.warn("Unable to find", id_to_move_to);
+        }
+        console.log(i, widget, jQuery(widget), classes, id_to_move_to);
+    });
+}
+
+fcmMove();
 
